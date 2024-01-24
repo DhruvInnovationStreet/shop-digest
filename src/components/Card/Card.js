a107f8d147a1be7c6061e133487fbf414d24c152
DhruvInnovationStreet <157638258+DhruvInnovationStreet@users.noreply.github.com>
2024-01-25 01:13:07 +0530

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,9 @@
+import Image from "next/image";
 export default function Card({ text }) {
   return (
     <div className="max-w-sm bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <img
+        <Image
           className=""
           src="https://flowbite.com/docs/images/blog/image-1.jpg"
           alt=""
