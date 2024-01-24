d6c730e95dd98b49212734e20ac9e7e0b1585a29
DhruvInnovationStreet <157638258+DhruvInnovationStreet@users.noreply.github.com>
2024-01-25 00:54:30 +0530

    changes made

---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+export default function Card({ text }) {
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          className=""
+          src="https://flowbite.com/docs/images/blog/image-1.jpg"
+          alt=""
+        />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </h5>
+        </a>
+      </div>
+    </div>
+  );
+}
